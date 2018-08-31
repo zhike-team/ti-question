e136cfc07362fb8cfc5f6c5f095755bf83208ea4
sunlu <sunlu@innobuddy.com>
2018-08-31 15:30:00 +0800

    update webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
-var path = require('path');
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-  mode: 'production',
   entry: './src/index.js',
+  devtool: 'source-map',
   output: {
     path: path.resolve(__dirname, 'lib'),
     filename: 'index.js'
@@ -15,8 +16,24 @@ module.exports = {
           path.join(__dirname, 'src'),
         ],
         use: ['babel-loader'],
+      }, {
+        test: /\.js$/,
+        include: path.join(__dirname, '../src'),
+        use: ['eslint-loader'],
+      }, {
+        test: /\.(png|jpg|gif|ttf)$/,
+        use: [{
+          loader: 'url-loader',
+        }],
       }
     ],
   },
-
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+    }),
+  ],
 };
