18195b80523aa69fbb3304e98d187c159259aa44
sunlu <sunlu@innobuddy.com>
2018-12-06 19:12:12 +0800

    fixed

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
-import Audio from './audio';
-import AudioPlayer from './audio_player';
-import Modal from './modal';
-import Recorder from './recorder';
-import Article from './article';
-import Block from './block';
+import AudioComponent from './audio';
+import AudioPlayerComponent from './audio_player';
+import ModalComponent from './modal';
+import RecorderComponent from './recorder';
+import ArticleComponent from './article';
+import BlockComponent from './block';
 
-export default {
-  Audio,
-  AudioPlayer,
-  Modal,
-  Recorder,
-  Article,
-  Block,
-};
+export const Audio = AudioComponent;
+export const AudioPlayer = AudioPlayerComponent;
+export const Modal = ModalComponent;
+export const Recorder = RecorderComponent;
+export const Article = ArticleComponent;
+export const Block = BlockComponent;
