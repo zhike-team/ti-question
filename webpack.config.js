2caad011cdccceb9c9b1a66b7dcda742a0d77aa0
sunlu <sunlu@innobuddy.com>
2018-12-07 11:42:06 +0800

    fixed

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './index.js',
   devtool: 'source-map',
   output: {
     path: path.resolve(__dirname, 'lib'),
