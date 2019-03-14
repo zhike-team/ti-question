aac812b3a4d55a78d34a3ad57c49ba8f8a80ff47
liujiawei <liujiawei@innobuddy.com>
2019-03-14 10:02:11 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './lib'),
     filename: 'index.js',
-    library: "tiComponent",
+    library: 'tiQuestion',
     libraryTarget: 'umd',
   },
   module: {
